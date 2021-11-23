bb03dd541a9b68f217fe99ba60b84a320d0cef42
Tuomas <en@kerro>
2021-11-23 15:40:46 +0200

    Initial Commit

---
+++ GeoSnake2/GeoSnake2App.swift
@@ -0,0 +1,20 @@
+//
+//  GeoSnake2App.swift
+//  GeoSnake2
+//
+//  Created by iosdev on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct GeoSnake2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
