f2c714eb3a8f544ebc316df19473383a70eb6d23
Diyar <ddogru10@gmail.com>
2021-12-13 16:14:57 +0200

    leaderboard layout alku

--- GeoSnake2/LeaderboardView.swift
+++ GeoSnake2/LeaderboardView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct LeaderboardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(uiImage: UIImage(named: "avatarplaceholder")!)
+                .resizable()
+                .clipped()
+                .clipShape(Circle())
+                .frame(width: 100, height: 100, alignment: .center)
+        }
     }
-}
 
 struct LeaderboardView_Previews: PreviewProvider {
     static var previews: some View {
         LeaderboardView()
+        }
     }
 }
